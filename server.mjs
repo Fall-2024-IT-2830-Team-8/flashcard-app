7f42ae949d8a1726123ea4078d4eacbda6424b13
creallfluharty <creallfluharty@gmail.com>
2024-12-03 13:41:55 -0600

    basic deck endpoints

--- server.mjs
+++ server.mjs
@@ -1,31 +1,52 @@
 import Database from 'better-sqlite3';
 import express from 'express';
+import bodyParser from 'body-parser';
+
 
 const PORT = 3000;
 
 
 function initializeSchema(db) {
-    db.prepare(
-        `CREATE TABLE IF NOT EXISTS User (
+    db.prepare(`
+        CREATE TABLE IF NOT EXISTS User (
             id INTEGER PRIMARY KEY,
             username TEXT,
             credentialHash TEXT
-        );`
-    ).run();
-    db.prepare(
-        `CREATE TABLE IF NOT EXISTS Deck (
+        );
+    `).run();
+    db.prepare(`
+        CREATE TABLE IF NOT EXISTS Deck (
             id INTEGER PRIMARY KEY,
             name TEXT
-        );`
-    ).run();
-    db.prepare(
-        `CREATE TABLE IF NOT EXISTS Card (
+        );
+    `).run();
+    db.prepare(`
+        CREATE TABLE IF NOT EXISTS Card (
             id INTEGER PRIMARY KEY,
             front TEXT, back TEXT,
             deck INTEGER,
             FOREIGN KEY(deck) REFERENCES Deck(id)
-        );`
-    ).run();
+        );
+    `).run();
+}
+
+function createDeck(db, name) {
+    const result = db.prepare(`
+        INSERT INTO Deck (name) VALUES (@name);
+    `).run({ name: name });
+
+    return result.lastInsertRowid;
+}
+
+function getDecks(db) {
+    const result = db.prepare(`
+        SELECT d.id, d.name, COUNT(c.id) as cardCount
+        FROM Deck d
+        LEFT JOIN Card c
+        on c.deck = d.id
+        GROUP BY d.id;
+    `).all();
+    return result;
 }
 
 app.get('/api/decks', (req, res) => {
@@ -60,9 +81,31 @@ function main() {
     initializeSchema(db);
 
     let app = express();
+    app.use(bodyParser.json());
 
     app.get('/', (request, response) => {
         response.sendFile('static/index.html', { root: '.' });
+    });
+
+    app.post('/api/decks', (request, response) => {
+        const initArgs = request.body;
+        console.log(initArgs);
+
+        if (initArgs.name === undefined) {
+            response.status(400);
+            response.json({ 'status': 'failure', 'error': 'Decks require a name.' });
+            return;
+        }
+
+        let deckId = createDeck(db, initArgs.name);
+        response.status(200);
+        response.json({ 'status': 'success', 'deckId': deckId });
+    });
+
+    app.get('/api/decks', (request, response) => {
+        let decks = getDecks(db);
+        response.status(200);
+        response.json({ 'status': 'success', 'decks': decks });
     });
 
     app.listen(PORT, () => console.log(`listening on port ${PORT}`));
