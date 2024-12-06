21fefd176dde9419f4de9cd3a0785cfd97e68d2c
creallfluharty <creallfluharty@gmail.com>
2024-12-05 18:51:52 -0600

    user API

---
+++ README.md
@@ -0,0 +1,8 @@
+usage
+```bash
+$ npm install .
+$ npx babel components/*.jsx -d static/
+$ node server.mjs
+```
+
+the app should now be available at `http://localhost:3000`
--- server.mjs
+++ server.mjs
@@ -10,7 +10,7 @@ function initializeSchema(db) {
     db.prepare(`
         CREATE TABLE IF NOT EXISTS User (
             id INTEGER PRIMARY KEY,
-            username TEXT,
+            username TEXT UNIQUE,
             credentialHash TEXT
         );
     `).run();
@@ -33,7 +33,7 @@ function initializeSchema(db) {
 function createDeck(db, name) {
     const result = db.prepare(`
         INSERT INTO Deck (name) VALUES (@name);
-    `).run({ name: name });
+    `).run({ name });
 
     return result.lastInsertRowid;
 }
@@ -52,7 +52,7 @@ function getDecks(db) {
 function createCard(db, deckId, front, back) {
     const result = db.prepare(`
         INSERT INTO Card (deck, front, back) VALUES (@deckId, @front, @back);
-    `).run({ deckId: BigInt(deckId), front: front, back: back });
+    `).run({ deckId: BigInt(deckId), front, back });
     return result.lastInsertRowid;
 }
 
@@ -61,7 +61,34 @@ function getCardsInDeck(db, deckId) {
         SELECT c.id, c.front, c.back
         FROM Card c
         WHERE c.deck = @deckId;
-    `).all({ deckId: deckId });
+    `).all({ deckId: BigInt(deckId) });
+    return result;
+}
+
+function createUser(db, username, credentialHash) {
+    const inserter = db.prepare(`
+        INSERT INTO User (username, credentialHash) VALUES (@username, @credentialHash);
+    `);
+
+    let userid;
+    
+    try {
+        const result = inserter.run({ username, credentialHash });
+        userid = result.lastInsertRowid;
+    } catch {
+        userid = null;
+    }
+
+    return userid;
+}
+
+function validateUser(db, username, credentialHash) {
+    const result = db.prepare(`
+        SELECT u.id
+        FROM User u
+        WHERE u.username = @username
+        AND u.credentialHash = @credentialHash;
+    `).all({ username, credentialHash });
     return result;
 }
 
@@ -86,19 +113,19 @@ function main() {
 
         if (name === undefined) {
             response.status(400);
-            response.json({ 'status': 'failure', 'error': 'Decks require a name.' });
+            response.json({ status: 'failure', error: 'Decks require a name.' });
             return;
         }
 
         let deckId = createDeck(db, name);
         response.status(200);
-        response.json({ 'status': 'success', 'deckId': deckId });
+        response.json({ status: 'success', deckId });
     });
 
     app.get('/api/decks', (request, response) => {
         let decks = getDecks(db);
         response.status(200);
-        response.json({ 'status': 'success', 'decks': decks });
+        response.json({ status: 'success', decks });
     });
 
     app.get('/api/decks/:deckId/cards', (request, response) => {
@@ -106,7 +133,7 @@ function main() {
 
         let cards = getCardsInDeck(db, deckId);
         response.status(200);
-        response.json({ 'status': 'success', 'cards': cards });
+        response.json({ status: 'success', cards });
     });
 
     app.post('/api/decks/:deckId/cards', (request, response) => {
@@ -119,13 +146,41 @@ function main() {
 
         if ([front, back].includes(undefined) || deckId === NaN) {
             response.status(400);
-            response.json({ 'status': 'failure', 'error': 'Card requires a deckId, front, and back.' });
+            response.json({ status: 'failure', error: 'Card requires a deckId, front, and back.' });
             return;
         }
 
         let cardId = createCard(db, deckId, front, back);
         response.status(200);
-        response.json({ 'status': 'success', 'cardId': cardId });
+        response.json({ status: 'success', cardId });
+    });
+
+    app.post('/api/users', (request, response) => {
+        const { action, username, credentialHash } = request.body;
+
+        if ([action, username, credentialHash].includes(undefined)) {
+            response.status(400);
+            response.json({ status: 'failure', error: 'Must specify action, username, credentialHash.' });
+            return;
+        }
+
+        if (action === 'create') {
+            const userid = createUser(db, username, credentialHash);
+            response.status(200);
+            response.json((userid === null)? { status: 'failure', error: 'username is already taken' } : { status: 'success' });
+            return;
+        }
+        
+        if (action === 'validate') {
+            const [ userid ] = validateUser(db, username, credentialHash);
+            response.status(200);
+            response.json({ status: (userid === undefined)? 'failure' : 'success'})
+            return;
+        }
+
+        response.status(400);
+        response.json({ status: 'failure', error: 'action must be one of create or validate' });
+        return;
     });
 
     app.listen(PORT, () => console.log(`listening on port ${PORT}`));
